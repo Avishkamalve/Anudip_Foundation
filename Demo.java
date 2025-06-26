fc624ac193fb109eecb1294f9f705230225b7ad0
avishkamalve <avshkamalve641@gmail.com>
2025-06-26 12:18:11 +0530

    Initial Commit 1

--- Demo.java
+++ Demo.java
@@ -5,6 +5,7 @@ public class Demo{
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println("Value of a is : "+a);
+        System.out.println("Hello World..!!");
         sc.close();
     }
 }

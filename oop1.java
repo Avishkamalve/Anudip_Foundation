303a4312d5d556c7c89e7b2b4a2a7dc8a89925d1
avishkamalve <avshkamalve641@gmail.com>
2025-07-03 11:19:36 +0530

    initial 2

---
+++ oop1.java
@@ -0,0 +1,22 @@
+class Pen{
+    
+        String color;
+        String type;
+        public void write(){
+        System.out.println("writing something");
+       
+
+    }
+    
+}
+public class oop1 {
+    public static void main(String[] args) {
+        Pen pen1=new Pen();
+        pen1.color="Blue";
+        pen1.type="ball";
+        pen1.write();
+
+        
+    }
+    
+}
